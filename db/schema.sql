f98cf3e6ddba3b3bdb58e4ac79528128bc0478ae
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-02-19 17:18:20 -0500

    schema and seeds working

--- db/schema.sql
+++ db/schema.sql
@@ -11,23 +11,23 @@ CREATE TABLE department (
 CREATE TABLE job (
   job_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(6,2),
-  dept_id INTEGER,
-    FOREIGN KEY (dept_id)
-    REFERENCES department (dept_id)
-    ON DELETE SET NULL
+  salary DECIMAL(9,2),
+  dept_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY
+    (dept_id) REFERENCES department(dept_id)
+      ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  emp_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  emp_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  job_id INTEGER,
-    FOREIGN KEY (job_id)
-    REFERENCES job (job_id)
-    ON DELETE SET NULL,
-  manager_id INTEGER,
-    FOREIGN KEY (emp_id)
-    REFERENCES employee (emp_id)
+  job_id INTEGER NOT NULL,
+  manager_id INTEGER UNSIGNED,
+    CONSTRAINT fk_job FOREIGN KEY
+    (job_id) REFERENCES job(job_id)
+    ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY
+    (manager_id) REFERENCES employee(emp_id)
     ON DELETE SET NULL
 );
